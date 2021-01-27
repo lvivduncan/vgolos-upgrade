7b6d07f8260370fba753c98d4d0ad88dcb53e3e6
Danylo Kolodiy <chaker@zdsimulator.com.ua>
2021-01-27 15:31:30 +0200

    Gallery fix

--- js/levus-horizontal-scroll.function.js
+++ js/levus-horizontal-scroll.function.js
@@ -4,50 +4,80 @@ function levusHorizontalScroll(selector){
     const scroll = document.querySelectorAll(selector);
 
     scroll.forEach(item => {
+        const gallery = item.parentNode;
+
         const ul = item.querySelector('ul');
         // elements
         let li = ul.querySelectorAll('li');
+        const cnt = li.length;
 
         // if less than 4, cloned 
-        if (li.length <= 4) {
+        if (cnt <= 4) {
             // cloned and append elements
             li.forEach(element => ul.append(element.cloneNode(true)));
             // new nodelist
             li = item.querySelectorAll('li');
         }
 
-        item.innerHTML += '<span class="left"></span><span class="right"></span>';
+        gallery.innerHTML += '<span class="left"></span><span class="right"></span>';
 
-        item.addEventListener('click', e => {
-            const ul = item.querySelector('ul');
-            if (e.target.className == 'left') {
-                // move last element
-                const last = ul.lastElementChild;
-                ul.prepend(last);
-                // destroy transition
-                ul.style.transition = 'none';
-                ul.classList.add('to-right');
-                setTimeout(() => {
-                    ul.classList.remove('to-right');
-                    ul.style.transition = '.5s';
-                }, 50);
+        const left_btn = gallery.querySelector('.left');
+        const right_btn = gallery.querySelector('.right');
+
+        const counter = gallery.querySelector('.gallery-counter');
+
+        if (counter)
+        {
+            let idx = 1;
+
+            counter.querySelector('.gallery-counter-total').innerHTML = cnt.toString();
+
+            const counter_current = counter.querySelector('.gallery-counter-current');
+            counter_current.innerHTML = idx.toString();
+
+            function updCounter(plus) {
+                if (plus)
+                    idx++;
+                else idx--;
+
+                if (idx < 1)
+                    idx = cnt;
+                else if (idx > cnt)
+                    idx = idx - cnt;
+
+                counter_current.innerHTML = idx.toString();
             }
+
+            left_btn.addEventListener('click', () => updCounter(false));
+            right_btn.addEventListener('click', () => updCounter(true));
+        }
+
+        left_btn.addEventListener('click', () => {
+            const ul = gallery.querySelector('ul');
+            // move last element
+            const last = ul.lastElementChild;
+            ul.prepend(last);
+            // destroy transition
+            ul.style.transition = 'none';
+            ul.classList.add('to-right');
+            setTimeout(() => {
+                ul.classList.remove('to-right');
+                ul.style.transition = '.5s';
+            }, 50);
         });
 
-        item.addEventListener('click', e => {
-            const ul = item.querySelector('ul');
-            if (e.target.className == 'right') {
-                // move first element
-                const first = ul.firstElementChild;
-                ul.append(first);
-                // destroy transition
-                ul.style.transition = 'none';
-                ul.classList.add('to-left');
-                setTimeout(() => {
-                    ul.classList.remove('to-left');
-                    ul.style.transition = '.5s';
-                }, 50);
-            }
+        right_btn.addEventListener('click', () => {
+            const ul = gallery.querySelector('ul');
+            // move first element
+            const first = ul.firstElementChild;
+            ul.append(first);
+            // destroy transition
+            ul.style.transition = 'none';
+            ul.classList.add('to-left');
+            setTimeout(() => {
+                ul.classList.remove('to-left');
+                ul.style.transition = '.5s';
+            }, 50);
         });
     });
 }
